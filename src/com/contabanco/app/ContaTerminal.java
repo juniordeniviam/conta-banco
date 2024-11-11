ac38b26b4fbdef2d1acb247f58656e0e8570a513
José Carlos <suportejr@outlook.com>
2024-11-11 18:32:17 -0300

    feat: Desafio Concluído!

--- src/com/contabanco/app/ContaTerminal.java
+++ src/com/contabanco/app/ContaTerminal.java
@@ -1,10 +1,41 @@
 package com.contabanco.app;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String boasVindas = ""
+				+ "---------------------------------------\n"
+				+ "| Bem-vindo ao Banco Digital Terminal |\n"
+				+ "---------------------------------------\n";
+		
+		Scanner sc = new Scanner(System.in);
+		
+        System.out.println(boasVindas);
+        System.out.println("Vamos simular o cadastro de conta bancaria com saldo \n");
+        
+        System.out.println("Por favor, digite seus dados e confirme com ENTER \n");
+        System.out.println("Nome: ");
+        String nome = sc.next();
+        
+        System.out.println("\nAgencia: ");
+        String agencia = sc.next();
+        
+        System.out.println("\nConta sem tracos: ");
+        int numero = sc.nextInt();
+        
+        System.out.println("\nPor favor, digite um valor para exibicao do saldo: ");
+        Double saldo = sc.nextDouble();
+		
+        sc.close();
+        
+        String contaCadastrada = "\n"
+        		+ "Ola "+ nome + ", obrigado por criar uma conta em nosso banco,"
+				+ " sua agencia e: " + agencia + ", conta: " + numero  
+				+ " e seu saldo: " + saldo + ", ja esta disponivel para saque.";
+        
+        System.out.println(contaCadastrada);
 	}
 
 }
